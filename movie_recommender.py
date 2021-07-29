163505f420fef8e696e3187534fc60d13bb1e4ee
Raghuvir Singh <raghuvir9923@gmail.com>
2021-07-29 13:11:55 +0530

    started building the engine

--- movie_recommender.py
+++ movie_recommender.py
@@ -26,5 +26,23 @@ def combine_features(row):
 
 df["combined_features"] = df.apply(combine_features, axis=1)
 
-print("Combined Features:", df["combined_features"].head())
+# print("Combined Features:", df["combined_features"].head())
+
+cv = CountVectorizer()
+
+count_matrix = cv.fit_transform(df["combined_features"])
+
+cosine_sim = cosine_similarity(count_matrix)
 movie_user_likes = "Avatar"
+
+movie_index = get_index_from_title(movie_user_likes)
+
+similar_movies = list(enumerate(cosine_sim[movie_index]))
+
+sorted_similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)
+i = 0
+for movie in sorted_similar_movies:
+    print(get_title_from_index(movie[0]))
+    i = i + 1
+    if i > 20:
+        break
