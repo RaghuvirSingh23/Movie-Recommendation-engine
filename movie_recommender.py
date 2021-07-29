f7568c88527b3179949c7e5760600220e78f354d
Raghuvir Singh <raghuvir9923@gmail.com>
2021-07-29 12:51:31 +0530

    started building the engine

---
+++ movie_recommender.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def get_title_from_index(index):
+    return df[df.index == index]["title"].values[0]
+
+
+def get_index_from_title(title):
+    return df[df.title == title]["index"].values[0]
+
+
+df = pd.read_csv("movie_dataset.csv")
+print(df.columns)
+features = ['keywords', 'cast', 'genres', 'director']
+
+
+def combine_features(row):
+    return row['keywords'] + " " + row['cast'] + " " + row['genres'] + " " + row['director']
+
+
+movie_user_likes = "Avatar"
+
+## Step 8: Print titles of first 50 movies
